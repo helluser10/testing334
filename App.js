7337b87702c9288313f52726df18d511e61adb8b
helluser10 <135004778+helluser10@users.noreply.github.com>
2024-10-13 02:47:59 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AIDictionary from './components/AIDictionary';
+import './styles/animations.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AIDictionary />
+    </div>
+  );
+}
+
+export default App;
